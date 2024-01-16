04186da222d8072b3ac4f95f255ace04bfb6d482
elvis-crespo <eacresposu@gmail.com>
2024-01-16 09:00:19 -0500

    second commit

--- weather-app/src/WeatherApp.jsx
+++ weather-app/src/WeatherApp.jsx
@@ -1,11 +1,13 @@
+import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-    height: 100vh;
+    /* height: auto; */
     /* background-image: url("/public/fondo.jpg"); */
     background-image: url("fondo.jpg");
+    min-height: 100vh;
     background-repeat: no-repeat;
     background-size: cover;
     display: flex;
@@ -110,8 +112,8 @@ const ContainerInput = styled.div`
 const WeatherContainer = styled.div`
     color: #FFF;
     background: #00000036;
-    top: 30%;
-    position: absolute;
+    /* top: 30%; */
+    /* position: absolute; */
     width: 90%;
     height: 60vh;
     display: flex;
@@ -119,6 +121,12 @@ const WeatherContainer = styled.div`
     align-items: center;
     flex-direction: row;
     border-radius: 10px;
+    backdrop-filter: blur(10px);
+    @media (width <= 600px ){
+        flex-direction: column;
+        height: 80vh;
+    }
+
     div{
         height: 100%;
         gap: 5px;
@@ -127,6 +135,7 @@ const WeatherContainer = styled.div`
         font-weight: bold;
         letter-spacing: 2px;
         font-size: 3.7vw;
+        border-radius: 10px;
         @media (width >= 200px ){
             font-size: 12px;
         }
@@ -151,8 +160,16 @@ const WeatherContainer = styled.div`
         p:nth-child(3) {
             font-family: 'Klee One', sans-serif;
             font-weight: bold;
-            font-size: 4vw;
             align-self: flex-end;
+            @media (width >= 200px ){
+                font-size: 25px;
+            }
+            @media (width >= 700px) {
+                font-size: 45px;
+            }
+            @media (width >= 1200px){
+                font-size: 55px;
+            }
         }
         p:nth-child(4){
             font-size: 3.5vw;
@@ -168,7 +185,7 @@ const WeatherContainer = styled.div`
             justify-self: center;
             @media (width >= 200px ){
                 width: auto;
-                height: 100px;
+                height: 130px;
             }
             @media (width >= 700px) {
                 width: auto;
@@ -200,7 +217,7 @@ const WeatherContainer = styled.div`
     }
     div:nth-child(2){
         background: #00000036;
-        width: 45%;
+        width: 100%;
         display: flex;
         justify-content: space-evenly;
         align-items: start;
@@ -271,6 +288,17 @@ export const WeatherApp = () => {
     const API_KEY = '6fa60d1a955b5de45f7b103e2a34459a'
     const keltoCen = 273.15;
 
+    const date = new Date();
+    const formattedDate = date.toLocaleDateString("en-US", {
+        weekday: 'long',
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+    }).replace(/,/, '.');
+    console.log(formattedDate);;
+    // console.log(formattedDate); 
+
+
     const handleInputChange = (e) => {
         setValue(e.target.value)
     }
@@ -289,12 +317,24 @@ export const WeatherApp = () => {
             } catch (error) {
                 if (error.response)
                     console.error('Respuesta del servidor con error:', error.response.data);
-                    console.error('Código de estado:', error.response.status);
-                    setError(error.response.data)
+                console.error('Código de estado:', error.response.status);
+                setError(error.response.data)
             }
             setPreviosValue(value);
         }
     }
+
+    const { isLoading, error: newError, data, refetch } = useQuery({
+        queryKey: ['weatherApp'],
+        queryFn: () =>
+            axios.get(`${URL_BASE}?q=${value}&appid=${API_KEY}`)
+        , onError: (error) => {
+            console.log(`${error} hello`)
+        }
+    })
+
+
+
 
     return (
         <>
@@ -320,14 +360,14 @@ export const WeatherApp = () => {
                     error ?
                         (
                             <InfoWeatherError>
-                                <p>{error.message}</p>
+                                <p>{error.message} </p>
                             </InfoWeatherError>
                         )
                         : dataWeather &&
                         (
                             <WeatherContainer>
                                 <div>
-                                    <p>September 25, 2024</p>
+                                    <p>{formattedDate}</p>
                                     <img
                                         src={`https://openweathermap.org/img/wn/${dataWeather.weather[0].icon}@2x.png`}
                                     />
@@ -347,7 +387,7 @@ export const WeatherApp = () => {
                                 <div>
                                     <span>
                                         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="40px" width="40px" xmlns="http://www.w3.org/2000/svg"><path d="M156.7 256H16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h142.2c15.9 0 30.8 10.9 33.4 26.6 3.3 20-12.1 37.4-31.6 37.4-14.1 0-26.1-9.2-30.4-21.9-2.1-6.3-8.6-10.1-15.2-10.1H81.6c-9.8 0-17.7 8.8-15.9 18.4 8.6 44.1 47.6 77.6 94.2 77.6 57.1 0 102.7-50.1 95.2-108.6C249 291 205.4 256 156.7 256zM16 224h336c59.7 0 106.8-54.8 93.8-116.7-7.6-36.2-36.9-65.5-73.1-73.1-55.4-11.6-105.1 24.9-114.9 75.5-1.9 9.6 6.1 18.3 15.8 18.3h32.8c6.7 0 13.1-3.8 15.2-10.1C325.9 105.2 337.9 96 352 96c19.4 0 34.9 17.4 31.6 37.4-2.6 15.7-17.4 26.6-33.4 26.6H16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16zm384 32H243.7c19.3 16.6 33.2 38.8 39.8 64H400c26.5 0 48 21.5 48 48s-21.5 48-48 48c-17.9 0-33.3-9.9-41.6-24.4-2.9-5-8.7-7.6-14.5-7.6h-33.8c-10.9 0-19 10.8-15.3 21.1 17.8 50.6 70.5 84.8 129.4 72.3 41.2-8.7 75.1-41.6 84.7-82.7C526 321.5 470.5 256 400 256z"></path></svg>
-                                        <p>&nbsp;  {dataWeather.wind.speed} npm</p>
+                                        <p>&nbsp;  {dataWeather.wind.speed} mph</p>
                                     </span>
                                     <span>
                                         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" version="1.1" id="Layer_1" viewBox="0 0 30 30" height="53px" width="53px" xmlns="http://www.w3.org/2000/svg"><path d="M7.56,17.19c0-0.88,0.24-1.89,0.72-3.03s1.1-2.25,1.86-3.31c1.56-2.06,2.92-3.62,4.06-4.67l0.75-0.72
@@ -377,9 +417,7 @@ export const WeatherApp = () => {
                             </WeatherContainer>
                         )
                 }
-
             </Container>
-
         </>
     )
 }
--- weather-app/src/main.jsx
+++ weather-app/src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { WeatherApp } from './WeatherApp.jsx'
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <WeatherApp />
+    <QueryClientProvider client={queryClient}>
+      <WeatherApp />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
